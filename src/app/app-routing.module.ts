7ab1faf0f6473747d5371fcfceae5dbfae1b23bc
maheeen-khan <maheeenkhan4@gmail.com>
2024-12-06 06:29:33 -0800

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+
+  //All routes are defined 
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, //it is neccessary to add, bcoz If no default route is set, Angular doesn't know what to display for the root path /.
+
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+// export const routingComponents = [HomeComponent, AboutComponent]
